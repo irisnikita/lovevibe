1bc8a1894d43f8c66692c1766c45e10c7c1fa95e
Vi Nguyen <vinlt@antsomi.com>
2024-03-30 20:30:50 +0700

    chore: udpate gtag

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,2 +1,4 @@
 export * from './theme';
 export * from './pokemon-card';
+
+export const gaTrackingId = 'G-NWZRPJ0G4Y';
--- app/root.tsx
+++ app/root.tsx
@@ -30,6 +30,7 @@ import {ConfigProvider} from './components/ConfigProvider';
 import LovevibeLogo from 'public/images/logos/lovevibe.png';
 import {Spin, Flex} from '~/components/ui';
 import {css} from '@emotion/css';
+import {gaTrackingId} from './constants';
 
 /**
  * This is important to avoid re-fetching root queries on sub-navigations
@@ -161,6 +162,40 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
+        {process.env.NODE_ENV !== 'development' && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            />
+            <script
+              async
+              id="gtag-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${gaTrackingId}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-M5ZNDP5Z');
+                `,
+              }}
+            />
+          </>
+        )}
       </head>
       <body>
         <ConfigProvider>
