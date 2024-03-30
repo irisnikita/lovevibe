337af5433898c12321b5aa9cc4a6974c4a4ebc56
Vi Nguyen <vinlt@antsomi.com>
2024-03-30 20:56:42 +0700

    chore: udpate gtag

--- app/components/lovevibe/Header.tsx
+++ app/components/lovevibe/Header.tsx
@@ -7,17 +7,20 @@ import {Button} from '~/components/ui';
 // Assets
 import LovevibeLogo from 'public/images/logos/lovevibe.png';
 import {ArrowRight} from '~/icons';
+import {Link} from '@remix-run/react';
 
 export function Header() {
   // const {shop, menu} = header;
   return (
     <header className="container flex items-center justify-between pt-8">
-      <Image
-        alt="lovevibe logo"
-        sizes="(min-width: 768px) 50vw, 100vw"
-        width={75}
-        src={LovevibeLogo}
-      />
+      <Link to="https://lovevibe.co" prefetch="intent">
+        <Image
+          alt="lovevibe logo"
+          sizes="(min-width: 768px) 50vw, 100vw"
+          width={75}
+          src={LovevibeLogo}
+        />
+      </Link>
       <Button
         type="link"
         href="https://lovevibe.co/pages/all-ideas"
