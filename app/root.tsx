6183b69a688174a3a26b872cf382bb4683467f86
Truong Vi <nltruongvi@gmail.com>
2024-08-10 16:06:15 +0700

    chore: update import

--- app/root.tsx
+++ app/root.tsx
@@ -162,6 +162,12 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta
+          httpEquiv="Cache-Control"
+          content="no-cache, no-store, must-revalidate"
+        />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
         <Seo />
         <Meta />
         <Links />
