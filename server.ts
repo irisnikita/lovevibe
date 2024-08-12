266c7e2e338537f0a89788a707f26a2c8797116e
Truong Vi <nltruongvi@gmail.com>
2024-08-12 12:10:23 +0700

    chore: update import

--- server.ts
+++ server.ts
@@ -125,6 +125,9 @@ export default {
 
       const response = await handleRequest(request);
 
+      response.headers.set('Cache-Control', 'no-store, max-age=0');
+      response.headers.set('Oxygen-Cache-Control', 'public, max-age=0');
+
       if (response.status === 404) {
         /**
          * Check for redirects only when there's a 404 from the app.
